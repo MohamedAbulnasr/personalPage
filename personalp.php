cb025aec1e2205b173caa38dfddec9b5958447ba
Mohamed Abulnasr <objectiveerp@gmail.com>
2017-12-10 01:42:50 +0200

    Add files via upload

---
+++ personalp.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mohamed Abulnasr Portfolio</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="Demo project with jQuery">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--	<link rel="stylesheet" href="../css/bootstrap.min.css"> 
+				<script src="../js/jquery-latest.min.js"></script> 
+				<link rel="stylesheet" href="../css/animate.min.css"> -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster|Satisfy|Roboto">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		
+		<style type="text/css">
+/*
+		transform: rotateY(180deg);
+		transition: 0.6s;
+		transform-style: preserve-3d;
+		position: relative;
+	.front, .back {
+		backface-visibility: hidden;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	.front {
+		z-index: 2;
+		transform: rotateY(0deg);
+	}
+	.back {
+		transform: rotateY(180deg);
+	}
+*/
+			.navbar{
+				font-size: x-large;
+				padding: 0px 10px 0px 10px;
+			}
+			.navbar > li {
+				float: right;
+			}
+			.navbar > li > a {
+				color: lightcyan;
+			}
+			.navbar > li > a:hover{
+				color: cyan;
+				background-color: #22AABB;
+				border-radius: 0 0 15px 15px;
+			}
+			.navbar > li > a:focus{
+				color: darkcyan;
+				background-color: #22B5B5;
+				border-radius: 20px;
+			}
+			.navbar-default{
+				background-color: #3399AA;
+				background:linear-gradient(45deg,cyan 25%,#3399AA 125%) ;
+				border-radius: 0% 0% 25% 25%;
+				border-width: 0px;
+				margin: 0;
+			}
+
+			#me{
+				background-color: blue;
+				background-image: -webkit-gradient( linear, left top, right top, color-stop(0, #1ae), color-stop(0.1, #22d), color-stop(0.5, #2ab),color-stop(0.7, #3b3), color-stop(1, #1a1) );
+			  color:transparent;
+			  -webkit-background-clip: text;
+			  background-clip: text;
+			  font-weight: bold;
+		}
+			#hd{
+				background-image:url("../images/1.jpg"), url("../images/13.jpg");
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-clip:content-box; 
+				background-blend-mode: soft-light;
+				background-size: 100%;
+				height: 660px;
+				margin: -20px 0px 0px 0px;
+				padding: 0px;
+			}
+			.wel{
+				padding-top: 50px;
+			}
+			.hdc{
+				font-family: Roboto ;
+				font-size: x-large;
+				color: white;
+			}
+			.hds{
+				font-family: Lobster;
+				font-size: xx-large;
+				color: white;
+				text-align: center;
+			}
+			.sph2{
+				font-family: 'Satisfy';
+				color: peru;
+			}
+			.sph3{
+				font-family: 'Satisfy';
+				font-size: large;
+				color: sienna;
+			}
+			#bio{
+				background-color: bisque;
+				background: linear-gradient(45deg, chocolate 15% ,bisque 100%);
+				height: 660px;
+				margin: 0px 0px 0px 0px;
+				padding: 0px;
+			}
+			#mypic{
+				text-shadow: gray;
+			}
+			#port{
+				background-color: lightcyan;
+				background:linear-gradient(45deg, cyan 25%, blue 125%);
+				height:660px;
+				margin: 0;
+				padding: 0;
+			}
+			.col-xs-4{
+				text-align: center;
+			}
+			#cont{
+				background-color: lightgray;
+				background:-webkit-linear-gradient(65deg, lightgray 25%, dimgray 125%);
+				background:-o-linear-gradient(65deg, lightgray 25%, dimgray 125%);
+				background:linear-gradient(155deg, lightgray 25%, dimgray 125%);
+				height:660px;
+				margin: 0;
+				padding: 0;
+			}
+			.bottom-footer{
+				background-color: #22AABB;
+				padding: 20px;
+				border-top: 1px solid cyan;
+				font-size: large;
+				color: black;
+			}
+			.bottom-footer > a {
+				color: darkblue;
+			}
+			.bottom-footer > a:hover {
+				background-color: #55B5B5;
+			}
+			.chevron1{
+				position: fixed;
+				z-index: 0;
+				-webkit-animation-duration: 3s;
+				-webkit-animation-delay: 1s;
+				-webkit-animation-iteration-count: infinite;
+
+				-o-animation-duration: 3s;
+				-o-animation-delay: 1s;
+				-o-animation-iteration-count: infinite;
+				-moz-animation-duration: 3s;
+				-moz-animation-delay: 1s;
+				-moz-animation-iteration-count: infinite;
+			}
+		</style>
+	</head>
+	<body>
+		<script type="text/javascript">
+			jQuery(function(){
+				$(".chevron1").css({
+					'top':$(window).height()-$(".chevron1").height()-15, 
+					'left':$(window).width()-$(".chevron1").width()-15
+				});
+				$(".chevron1 a").css({
+					'color': 'darkcyan'
+				});
+				$(".chevron1").addClass('class_name')
+
+				$("#mypic1").hover(function() {
+					$("#mypic1").addClass('pulse');
+				}, function() {
+					$("#mypic1").removeClass('pulse');
+				});
+
+				$("#mypic2").hover(function() {
+					$("#mypic2").addClass('tada');
+				}, function() {
+					$("#mypic2").removeClass('tada');
+				});
+
+				$("#mypic3").hover(function() {
+					$("#mypic3").addClass('pulse');
+				}, function() {
+					$("#mypic3").removeClass('pulse');
+				});
+				$("#mypic").hover(function() {
+					$("#mypic").addClass('tada');
+				}, function() {
+					$("#mypic").removeClass('tada');
+				});
+
+				$('a[href^="#"]').click(function() {
+			    event.preventDefault();
+			    $('html').animate({
+			      scrollTop: $(this.hash).offset().top
+			     }, 1000);
+			  });
+
+			});
+		</script>
+		<nav class="navbar navbar-default" id="tops">
+		  <div class="container-fluid">
+		    <div id="myNavbar">
+		      <ul class="nav navbar navbar-left">
+		        <li class="active"><a id="me" href="#tops">Mohamed Abulnasr</a></li>
+		      </ul>
+		      <ul class="nav navbar navbar-right">
+		        <li class=""><a href="#cont">Contact</a></li>
+		        <li class=""><a href="#port">Portfolio</a></li>
+		        <li class=""><a href="#bio">Bio</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+		<div class="chevron1 animated infinite slideOutUp">
+			<a href="#tops"><i class="fa fa-chevron-circle-up fa-5x" aria-hidden="true"></i></a>
+		</div>
+		<header id="hd">
+			<div class="container-fluid wel">
+				<div class="col-xs-5 hdc">
+					Deep Expertise in Business applications
+					<br>
+					Life Cycle E.R.P Senior Developer
+					<br>
+					Full stack Senior Developer
+					<br>
+				</div>
+				<div class="col-xs-2"></div>
+				<div class="col-xs-5 hdc">
+				<i><u>Over 15 years :</u></i>
+				<br>
+					&nbsp;&nbsp;<i class="fa fa-magic"></i> Develop unprecedent solutions to overcome complex problems. 
+				<br>
+					&nbsp;&nbsp;<i class="fa fa-magic"></i> Analyze, Full Design, Full Develop and Implement Integrated Application for Business Finance.
+				<br>
+				</div>
+			</div>
+		</header>
+		<div class="container-fluid">
+			<div id="bio">
+				<div class="hds" >
+				Bio<span class="sph2">graphy</span>: 
+				<br>
+				</div>
+				<div class="img-responsive col-xs-3">
+					<img id="mypic" class="img-rounded animated " src="../images/12.jpg" alt="myPic">
+				</div>
+				<div class="col-xs-7">
+					<p class="hdc">My name is <span class="sph3">Abulnasr, Mohamed A. 
+						<br/>About my family; Married and have children.
+						<br/>We're settled in Egypt nowadays and <i class="fa fa-smile-o"></i> enjoy.</span>
+					</p>
+				</div>
+				<div class="col-xs-7">
+					<p class="hdc">My Education is <span class="sph3">in both Accounting finance and Software development. 
+						<br/>I Graduate from Faculty of Commerce in Cairo University, Egypt.
+						<br/>I've Certifications of MCPD and MCSD from Microsoft in Web Application and Solution Development.</span>
+					</p>
+				</div>
+				<div class="col-xs-7">
+					<p class="hdc">My Career <span class="sph3">during 15 years thrwogh 3 companies inside Saudia Arabia and Egypt. i finalized several Software projects and provided alot of solutions of problems in several systems serving Financial, Production, Business Management and Web sites. and fulfill the Specific requests of unusual problems.</span>
+					</p>
+				</div>
+			</div>
+			<div id="port">
+				<div class="hds">
+					Portfolio:
+					<br/>
+				</div>
+				<div class="hdc text text-center">
+					These sites are a fast sample of my projects.
+					<br/>
+					<br/>
+				</div>
+				<div class="col-xs-4">
+					<div id="derasat1"><a href="http://derasatdomyat.com">
+						<img id="mypic1" class="img-rounded animated picpro" src="../images/derasatlogoxs.png" alt="Pic of Derasat Site"></a>
+					</div>
+				</div>
+
+				<div id="bara3im1" class="col-xs-4">
+					<img id="mypic2" class="img-rounded animated picpro" src="../images/bara3imxs.png" alt="Pic of Bara3im Site">
+				</div>
+				<div class="col-xs-4">
+					<div id="empty1">
+						<img id="mypic3" class="img-rounded animated picpro" src="../images/empty1.png" alt="Empty Pic">
+					</div>
+				</div>
+			</div>
+			<div id="cont">
+				<div class="hds">
+					Contact: 
+					<br/>
+				</div>
+				<div class="hdc text text-center">
+					Feel free to contact me. And i promise to reply A.S.A.P.
+				<br/>
+				<br/>
+				</div>
+				<div class="col-xs-1"></div>
+				<div class="col-xs-4 form-group">
+					<form method="POST" action="">
+						<input type="text" name="nm" id="nm" class="form-control input-lg" placeholder="Name" required />
+						<br/>
+						<input type="text" name="mail" class="form-control input-lg" placeholder="Email Address" required />
+						<br/>
+						<input type="text" name="phone" class="form-control input-lg" placeholder="Phone Number"/>
+						<br/>
+						<input type="text" name="message" class="form-control input-lg" placeholder="Message" required />
+						<br/>
+						<button type="submit" name="submit" class="form-control input-lg btn btn-primary btn-lg"/><i class='fa fa-paper-plane'></i> Send</button>
+					</form>
+				</div>
+				<div class="col-xs-7">
+					<div class="hdc text text-center">
+						Social Accounts related to me :
+						<br/><br/>
+					<div class="btn-group">
+						<div class="btn btn-default btn-lg"><a href="https://twitter.com/mynasr_mohamed" target="_blank">
+							<i class="fa fa-twitter fa-2x"></i></a>
+						</div>
+						<div class="btn btn-default btn-lg"><a href="https://www.linkedin.com/in/mohamedabulnasr" target="_blank">
+							<i class="fa fa-linkedin fa-2x"></i></a>
+						</div>
+						<div class="btn btn-default btn-lg"><a href="https://github.com/MohamedAbulnasr" target="_blank">
+							<i class="fa fa-github fa-2x"></i></a>
+						</div>
+						<div class="btn btn-default btn-lg cn"><a href="https://www.facebook.com/objectivesystems" target="_blank">
+							<i class="fa fa-facebook-official fa-2x"></i></a>
+						</div>
+						<div class="btn btn-default btn-lg"><a href="https://stackoverflow.com/users/7637591/mohamed-abulnasr" target="_blank">
+							<i class="fa fa-stack-overflow fa-2x"></i></a>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+			<footer id="footer" class="bottom-footer text-center">
+				Written and coded by <a href="https://www.freecodecamp.org/mohamedabulnasr" target="_blank">Mohamed Abulnasr 	<img id="forimg" src="https://raw.githubusercontent.com/FutureForObjectiveSystems/Albara3im/master/minlogo2.png" width="56px"
+            style="border:4px ;vertical-align:middle;"
+            /></a> <i class="fa fa-copyright"></i> 2017. All Rights Reserved
+			</footer>
+		</div>
+	</body>
+</html>
